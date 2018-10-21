189cede44b0bb5c8bff9537cf5d12840bf300deb
bferguson3 <32964659+bferguson3@users.noreply.github.com>
2018-10-21 01:42:20 -0700

    yyyy

--- nestest.asm
+++ nestest.asm
@@ -59,29 +59,6 @@ pal_loop:
         cpx #255
         bne .FillLoop    ; fills screen with tile #20
 
-        lda #$21
-        sta $2006
-        lda #$20        ; skip 64 lines - 2 row.
-        sta $2006
-        ldx #0
-        ldy #2
-.FillLoop2:
-        sty $2007
-        inx
-        cpx #200
-        bne .FillLoop2    ; fills screen with tile #20
-
-        lda #$22
-        sta $2006
-        lda #$20        ; skip 64 lines - 2 row.
-        sta $2006
-        ldx #0
-        ldy #0
-.FillLoop3:
-        sty $2007
-        inx
-        cpx #255
-        bne .FillLoop3    ; fills screen with tile #20
 
 
         lda #$23
